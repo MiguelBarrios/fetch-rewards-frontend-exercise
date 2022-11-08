e3ff4503ea3bf3a66bf12c904810aa36b53ce699
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-11-08 17:00:49 -0600

    created function to send create new user request

--- resources/scripts/createUser.js
+++ resources/scripts/createUser.js
@@ -21,3 +21,42 @@ function loadOccupationAndStateData(){
     }
     xhr.send();
 }
+
+function createNewUser(){
+
+}
+
+function sendCreateNewUserRequest(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://frontend-take-home.fetchrewards.com/form', true)
+    xhr.setRequestHeader('Content-type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 ) {
+          if ( xhr.status == 200 || xhr.status == 201 ) { 
+            let data = JSON.parse(xhr.responseText);
+            console.log("Success")
+            console.log(data);
+          }
+          else {
+            console.error("POST request failed.");
+            console.error(xhr.status + ': ' + xhr.responseText);
+          }
+        }
+    };
+      
+    let userObject = {
+        "name": "Jimmathy A Smith",
+        "email": "jim@gmail.com",
+        "password": "password",
+        "occupation": "Head of Shrubbery",
+        "state": {
+            name: 'Alabama', 
+            abbreviation: 'AL'
+        }
+    };
+      
+    let userObjectJson = JSON.stringify(userObject); 
+      
+    xhr.send(userObjectJson);
+}
