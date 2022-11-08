e194ce6df96872cf24ea6b7699e7336050990a8b
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-11-08 17:41:31 -0600

    Added autocomplete to state and occupation fields in create user form

--- resources/scripts/createUser.js
+++ resources/scripts/createUser.js
@@ -1,31 +1,4 @@
 
-let states = [];
-let occupations = []
-
-function loadOccupationAndStateData(){
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('GET', 'https://frontend-take-home.fetchrewards.com/form');
-
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4){
-            if(xhr.status == 200){
-                let data = JSON.parse(xhr.responseText);
-                states = data.states;
-                occupations = data.occupations;
-            }
-            else{
-                console.log(xhr.status + " : " +xhr.responseText);
-            }
-        }
-    }
-    xhr.send();
-}
-
-function createNewUser(){
-
-}
-
 function sendCreateNewUserRequest(){
     let xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://frontend-take-home.fetchrewards.com/form', true)
